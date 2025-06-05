b0dd4746f85eecc7f815cc4fca7624235e7de475
Aziret <sharshenbaevaziret@gmail.com>
2025-06-05 15:35:50 +0600

    fixmedia root

--- mykitten/settings.py
+++ mykitten/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_ROOT = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
